99b9cbb4423e6e40e9a8f8911818b69960224aa0
mynul <homeexercise8@gmail.com>
2022-05-08 04:25:58 +0600

    some responsive done

--- src/Components/Companies/Companies.js
+++ src/Components/Companies/Companies.js
@@ -1,13 +1,25 @@
 import React from 'react';
-
+import { Col, Container, Row } from 'react-bootstrap';
 const Companies = () => {
     return (
-        <div className='d-flex mt-5 mb-5 justify-content-center '>
-            <img width={100} src="https://i.ibb.co/R27Yr4f/1813.png" alt="" />
-            <img  className='ms-5' width={100} src="https://i.ibb.co/dQr8fG8/1513.png" alt="" />
-            <img  className='ms-5' width={100} src="https://i.ibb.co/mc34qjb/1612.png" alt="" />
-            <img  className='ms-5' width={100} src="https://i.ibb.co/TvcQHqW/main-qimg-3e0db2de7e1d8b4cebce5df3cad338e7.png" alt="" />
-        </div>
+        <Container fluid>
+            <Row xs={2} lg={4} className="py-5 ">
+                <Col className="d-flex justify-content-center">
+                    <img width={100} src="https://i.ibb.co/R27Yr4f/1813.png" alt="" />
+                </Col>
+                <Col className="d-flex justify-content-center">
+                    <img className='ms-5' width={100} src="https://i.ibb.co/dQr8fG8/1513.png" alt="" />
+                </Col>
+                <Col className="d-flex justify-content-center">
+                    <img className='ms-5' width={100} src="https://i.ibb.co/mc34qjb/1612.png" alt="" />
+                </Col>
+                <Col className="d-flex justify-content-center">
+                    <img className='ms-5' width={100} src="https://i.ibb.co/TvcQHqW/main-qimg-3e0db2de7e1d8b4cebce5df3cad338e7.png" alt="" />
+                </Col>
+
+            </Row>
+
+        </Container>
     );
 };
 
